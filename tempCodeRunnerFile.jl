c6c2bef41293008e47a13ee5dfaf426bc885f53c
Ahmed-Yassine CHRAA <102172578+AhmedYassi02@users.noreply.github.com>
2024-10-31 00:14:44 +0100

    Delete tempCodeRunnerFile.jl

--- tempCodeRunnerFile.jl
+++
@@ -1,7 +0,0 @@
-    for i in 1:nombre_aerodromes
-        for j in 1:nombre_aerodromes
-            if distances[i, j] > rayon
-                @constraint(model, x[i, j] == 0)
-            end
-        end
-    end
